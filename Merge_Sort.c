820f2e7a810fa7ad2b01a71d94ab41eb1520ebdb
madhav <113962975+192110263@users.noreply.github.com>
2023-02-06 12:52:27 +0530

    Update Merge_Sort.c

--- Merge_Sort.c
+++ Merge_Sort.c
@@ -34,3 +34,47 @@ void merge_Sort(int a[], int low, int mid, int high)
 	{
 		for (k = lo; k <= mid; k++)
 		{
+		temp[i] = a[k];
+			i++;
+		}
+	}
+  
+	for (k = low; k <= high; k++)
+		a[k] = temp[k];
+}
+
+
+void partition(int a[],int low,int high)
+{
+      int mid;
+     
+	  if(low < high)
+      {
+		mid = (low + high)/2;
+		
+		partition( a, low, mid);
+		
+		partition(a, mid+1, high);
+		
+		merge_Sort(a, low, mid, high);
+	  }
+}
+
+
+
+
+int main()
+{
+      int a[50] ,  i, n;
+      printf("Enter total number of elements:");
+      scanf("%d", &n);
+      printf("Enter the elements:\n");
+      for(i = 0; i < n; i++)
+          	scanf("%d", &a[i]);
+
+      partition( a, 0, n - 1);
+	   
+      printf("After merge sort:\n");
+      for(i = 0;i < n; i++)
+	 		printf("%d\t", a[i]);
+}
